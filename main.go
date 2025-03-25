d01d048f3135a34e1785f4464cef73cfd963feae
FKD13 <44001949+FKD13@users.noreply.github.com>
2025-03-25 23:56:53 +0100

    start running fping and parsing output

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
+	"log"
 	"math/rand"
+	"os/exec"
+	"regexp"
 	"time"
 )
 
@@ -19,6 +23,11 @@ type Target struct {
 	IP          string
 	fails       int
 	UIComponent *TargetComponent
+
+	LastState   string
+	LastPing    float64
+	AvgPing     float64
+	PercentLoss int
 }
 
 func NewTargetComponent(ipString string) *TargetComponent {
@@ -58,7 +67,45 @@ func (t *TargetComponent) Update(grid *tview.Grid, row int, column int) {
 	grid.AddItem(t.Flex, row, column, 1, 1, 1, 1, false)
 }
 
+func RunFPing() {
+	cmd := exec.Command("fping", "-l", "127.0.0.1", "1.1.1.1")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		panic(err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		panic(err)
+	}
+
+	// Failed pings contain "timed out"
+	reFail := regexp.MustCompile(`^(([0-9]+\.){3}[0-9]+) +: \[[0-9]+], timed out \(([0-9]*\.[0-9]*|NaN) avg, ([0-9]+)% loss\)$`)
+	// Successful pings contain "bytes"
+	reSuccess := regexp.MustCompile(`^(([0-9]+\.){3}[0-9]+) +: \[[0-9]+], [0-9]+ bytes, ([0-9]*\.[0-9]*) ms \(([0-9]*\.[0-9]*) avg, ([0-9]+)% loss\)$`)
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		if reFail.MatchString(scanner.Text()) {
+			fmt.Println("Failed")
+			fmt.Println(reFail.FindAllStringSubmatch(scanner.Text(), -1))
+
+		} else if reSuccess.MatchString(scanner.Text()) {
+			fmt.Println("Success")
+			fmt.Println(reSuccess.FindAllStringSubmatch(scanner.Text(), -1))
+		} else {
+			panic("target state unclear")
+		}
+	}
+
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
+
+	go RunFPing()
+
+	time.Sleep(time.Second * 10)
 
 	comp1 := NewTargetComponent("1.1.1.1")
 	comp2 := NewTargetComponent("8.8.8.8")
